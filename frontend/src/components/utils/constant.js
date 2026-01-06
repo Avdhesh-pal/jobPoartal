29d25c0cda515de3e1dc9abc20f211ac03a5f200
Avdhesh Pal <pal709412@gmail.com>
2026-01-06 23:18:20 +0530

    error correction

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,5 +1,5 @@
 
-export const USER_API_END_POINT = "https://jobpoartal-3.onrender.com/v1/user";
+export const USER_API_END_POINT = "https://jobpoartal-3.onrender.com/api/v1/user";
 export const JOB_API_END_POINT =  "https://jobpoartal-3.onrender.com/api/v1/job"
 export const APPLICATION_API_END_POINT =  "https://jobpoartal-3.onrender.com/api/v1/application"
 export const COMPANY_API_END_POINT =  "https://jobpoartal-3.onrender.com/api/v1/company"
