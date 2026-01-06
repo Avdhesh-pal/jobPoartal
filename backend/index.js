08a008670f1e80adcbb8e98962df3a5a8a22076a
Avdhesh Pal <pal709412@gmail.com>
2026-01-06 19:27:46 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 const corsOption ={
-    origin :"http://localhost:5173",
+    origin :"https://jobpoartal-3.onrender.com",
     credentials :true
 }
 app.use(cors(corsOption));
