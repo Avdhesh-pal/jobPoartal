1f04f4976c5d238ae636223ad05b6128fd1105f6
Avdhesh Pal <pal709412@gmail.com>
2026-01-06 22:32:33 +0530

    error correction

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,10 @@ import path from "path";
 dotenv.config({})
 
 const app = express();
- const _dirname = path.resolve();
+const _dirname = path.resolve();
 
 // middlewares
+app.set("trust proxy", 1); 
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static("public"));
